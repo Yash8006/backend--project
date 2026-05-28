1b1534d1402bcca2d0a7d4f33039ef985124200f
Yash Kumar Goyal <ykg@Yashs-MacBook-Air.local>
2026-05-28 16:58:29 +0530

    error handling

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
 // require("dotenv").config();
 import dotenv from "dotenv";
+import app from "./app.js";
 dotenv.config({
     path: "./env"
 });
 import express from "express";
 import { connectToDatabase } from "./db/db.js";
-const app = express();
-connectToDatabase();
+
+connectToDatabase().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+    
+}).catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the process with an error code
+});
 
 
 
-// (async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URL, {
-//       dbName: DB_NAME,
-//     });
-//     console.log("Connected to MongoDB");
-//     app.on("error", (error) => {
-//       console.error("Error starting the server:", error);
-//     });
-//     app.listen(process.env.PORT, () => {
-//       console.log(`Server is running on port ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error);
-//   }
-// })();
